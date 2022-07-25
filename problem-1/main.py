084d1f402fec8fc34ad4f87145b365c8aa82c921
Estela <estela.martinez.calo@cern.ch>
2022-07-25 22:50:32 +0200

    Final

--- problem-1/main.py
+++ problem-1/main.py
@@ -5,10 +5,9 @@
 
 
 # Boolean variable to allow while loop to continue
-# Variable booleana para permitir que el bucle while continúe
 continueLoop = True
 
-#resumir
+#the summarize function is not used at any time
 def summarize(clist, testNumber):
     fname = "summary" + str(testNumber)
     print()
@@ -34,48 +33,42 @@ figures_registry = []
 
 
 class Cube(object):
-    def to_str(self) -> str:
-        return "Cube: Volume:" + str(self.volume **3)
-    def __init__(self, v: int):
+    def __init__(self, v):
         self.volume = v
+
+    def to_str(self):
+        return "Cube: Volume:" + str(self.volume ** 3)
 
 
 class Circle(object):
-    def to_str(self) -> str:
-        return "Circle: Radius:" + str(self.radius) + " Area:" + str(pi * self.radius ** 2) + " Perimeter:" + str(
-            2 * pi * self.radius)
-    def __init__(self, r: int):
+    def __init__(self, r):
         self.radius = r
+
+    def to_str(self):
+        return "Circle: Radius: " + str(self.radius) + " Area: " + str(pi * self.radius ** 2) + " Perimeter: " + str(
+            2 * pi * self.radius)
 
 class Square(object):
     def __init__(self, s):
         self.side = s
-    def to_str(self):
-        return "Square: Radius:" + str(self.side) + " Area:" + str(self.side ** 2) + " Perimeter:" + str(self.side * 4)
 
+    def to_str(self):
+        return "Square: Radius: " + str(self.side) + " Area: " + str(self.side ** 2) + " Perimeter: " + str(
+            self.side * 4)
 
 
 def main():
     print("Hello!")
 
-    # Formatting the input shape to allow the input of upper/lowercase as well as odd spacing
-    # Formatear la forma de entrada para permitir la entrada de mayúsculas/minúsculas así como el espaciado impar
-    def format_input(text_line):
-        text_line = text_line.lower().strip()
-        word_list = text_line.split()
-        text_line = " ".join(word_list)
-        return text_line
-
-
     testNumber = input("What is the test number? ")
 
-    while continueLoop:
+    while True:
         # prompts user for the shape
         # solicita al usuario la forma
         shape = input(
-            "What shape would you like to calculate the metrics of? Input \"c\" for  \"cube\", \"q\" for \"quit\", \"ci\" for \"circle\",  \"s\" for \"square\": "
-        )
-        shape = format_input(shape)
+            "What shape would you like to calculate the metrics of? Input \"c\" for  \"cube\", \"ci\" for \"circle\", "
+            "\"s\" for \"square\", \"q\" for \"quit\": ").lower().strip()
+
         if shape == "q":
             print("You have reached the end of your session.")
             if not figures_registry:
